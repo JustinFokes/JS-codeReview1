c828fdfb94e52f753bfe16a36b135354387d6973
jf <j.k.fokes@gmail.com>
2016-06-24 13:58:05 -0700

    clearing fields

--- js/repo-interface.js
+++ js/repo-interface.js
@@ -7,20 +7,17 @@ var apiKey = require('./../.env').apiKey;
 //Below will be placed variables to be passed into our object.
 
 
-
 var getName = function(userName){
-	$('#results').append("<h5>" + userName + "</h5>");
+	$('#results').html("<h5>" + userName + "</h5>");
 }
 
 var getAvatar = function(picture){
 
-	$('#avatar').append("<br>" + "<img class='img-circle' src="+ picture +"/>");
+	$('#avatar').html("<br>" + "<img class='img-circle' src="+ picture +"/>");
 
 }
 
 var getRepositoryList = function(list, repoUrl){
-
-
 	$('#reposList').append("<a href="+repoUrl+">"+list+"</a>" + "<br>");
 
 }
@@ -32,12 +29,14 @@ var getRepositoryList = function(list, repoUrl){
 
 $(document).ready(function(){
 
-
+		
 
 	//Below will be our user-logic.
 
 	$('#find').click(function(){
+		$('#reposList').empty();
 		var name = $('#userName').val();
+		$('#userName').val("");
 		var newRepo = new Repo(name);
 		newRepo.getRepos(getName, getAvatar)
 		newRepo.getRepoList(getRepositoryList);
